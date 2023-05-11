3d8e17a6e777fb9ccf2a252e43d4a850c5507615
Juan <jjcaimen@gmail.com>
2023-05-10 20:28:10 -0400

    add socket in types

--- controllers/type.js
+++ controllers/type.js
@@ -2,7 +2,7 @@ const { validationResult } = require("express-validator");
 const { validationParams } = require("../utils/validationParams");
 const { errorHandler } = require("../utils/errorHandler");
 const Type = require("../models/type");
-const type = require("../models/type");
+const io = require("../socket/socket");
 
 exports.getAllTypes = async (req, res, next) => {
   try {
@@ -68,6 +68,14 @@ exports.insertType = async (req, res, next) => {
       lastAuthor: lastAuthor,
     });
     const result = await type.save();
+    const returnType = {
+      name: name,
+      _id: result._id,
+    };
+    io.getIO().emit("type", {
+      action: "create",
+      type: { ...returnType, creator: { _id: req.userId } },
+    });
     res.status(201).json({ message: "OK", isSaved: true, typeId: result._id });
   } catch (err) {
     errorHandler(err, next);
@@ -80,13 +88,21 @@ exports.updateType = async (req, res, next) => {
     validationParams(res, errors);
     const typeId = req.params.typeId;
     const name = req.body.name;
-    const typeObject = await type.findById(typeId);
+    const typeObject = await Type.findById(typeId);
     if (!typeObject) {
       throw new Error("This type dont exist anymore");
     }
     typeObject.name = name;
     typeObject.lastAuthor = req.userId;
     const result = await typeObject.save();
+    const returnType = {
+      name: name,
+      _id: result._id,
+    };
+    io.getIO().emit("type", {
+      action: "update",
+      type: returnType,
+    });
     res.status(201).json({ message: "OK", isSaved: true, typeId: result._id });
   } catch (err) {
     errorHandler(err, next);
@@ -98,12 +114,15 @@ exports.deleteType = async (req, res, next) => {
     const errors = validationResult(req);
     validationParams(res, errors);
     const typeId = req.params.typeId;
-    const typeObject = await type.findById(typeId);
-
+    const typeObject = await Type.findById(typeId);
     if (!typeObject) {
       throw new Error("This type dont exist anymore");
     }
     await Type.findByIdAndDelete(typeId);
+    io.getIO().emit("type", {
+      action: "delete",
+      type: typeId,
+    });
     res.status(201).json({ message: "OK", isDeleted: true });
   } catch (err) {
     errorHandler(err, next);
